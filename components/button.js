a674d50adc7e2de27f904b4b13c1430e62179a95
洪詣軒 <simon@MacBook-Pro.local>
2021-11-21 16:24:36 +0800

    minior chage

---
+++ components/button.js
@@ -0,0 +1,9 @@
+// import styled from 'styled-components'
+
+export default function Button() {
+  return (
+    <div>
+      <button>BUTTON</button>
+    </div>
+  )
+}
--- components/header.js
+++ components/header.js
@@ -1,3 +1,24 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+
+const StyledHeader = styled.nav`
+  background-color: black;
+  text-align: center;
+`
+
+const StyledALink = styled.a`
+  color: white;
+`
+
 export default function Header(props) {
-  return <div>HEADER</div>
+  return (
+    <StyledHeader>
+      <Link href="/" passHref>
+        <StyledALink>page1</StyledALink>
+      </Link>
+      <Link href="/" passHref>
+        <StyledALink>page2</StyledALink>
+      </Link>
+    </StyledHeader>
+  )
 }
